995096f9bf2262bc50e2093734231325788baf3d
Ben Zhang <benzh@cs.berkeley.edu>
2017-09-24 15:55:58 -0700

    use struct opt

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,29 @@
 extern crate mdp;
-extern crate getopts;
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use std::fs::File;
 use std::io::Read;
 use std::io::Result;
-use std::fs::File;
 use std::string::String;
-use std::env;
-use getopts::Options;
+use structopt::StructOpt;
 
-fn print_usage(opts: Options) {
-    let brief = format!("Usage: mdp [OPTION]... [FILE]");
-    print!("{}", opts.usage(&brief));
+#[derive(StructOpt, Debug)]
+#[structopt(name = "mdp", about = "A markdown presentation tool in Rust.")]
+struct Opt {
+    /// Path to the markdown file.
+    #[structopt(help = "Markdown file")]
+    file: String,
 }
 
 fn main() {
-    run().unwrap();
+    let opt = Opt::from_args();
+    run(opt).unwrap();
 }
 
-fn run() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let mut opts = Options::new();
-    opts.optflag("h", "help", "print the usage");
-    opts.reqopt("f", "file", "markdown file to present", "presen.md");
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(_) => panic!("invalid argument"),
-    };
-    if matches.opt_present("h") {
-        print_usage(opts);
-        return Ok(());
-    }
-    let filename = matches.opt_str("f").expect("You need to provide a file to present");
-    let mut f = try!(File::open(filename));
+fn run(opt: Opt) -> Result<()> {
+    let mut f = try!(File::open(opt.file));
     let mut s = String::new();
     try!(f.read_to_string(&mut s));
     println!("{}", s);
